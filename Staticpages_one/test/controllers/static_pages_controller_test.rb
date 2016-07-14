afdac23770ad9f9e1c95cba869a19a739fad7181
astronautaCero <jffersosa@gmail.com>
2016-07-14 11:45:15 -0500

    14/07/2016-11:45am

--- Staticpages_one/test/controllers/static_pages_controller_test.rb
+++ Staticpages_one/test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,20 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
   test "should get welcome" do
     get static_pages_welcome_url
-    assert_response :success
+    assert_response :success #Afirmar la respuesta si es true
+    assert_select "h1", "View Welcome" #Me comprueba si en la etiqueta este el contenido (true)
   end
 
   test "should get help" do
     get static_pages_help_url
+    assert_response :success
+  end
+
+   test "should get otro" do
+    get static_pages_otro_url
     assert_response :success
   end
 
