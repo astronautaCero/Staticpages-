74c47dcf0dbb449445635771704b963f8aac643a
astronautaCero <jffersosa@gmail.com>
2016-07-15 12:00:59 -0500

    15/07/2016-12:00pm

--- Staticpages_one/test/controllers/static_pages_controller_test.rb
+++ Staticpages_one/test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  
+
   test "should get welcome" do
     get static_pages_welcome_url
     assert_response :success #Afirmar la respuesta si es true
